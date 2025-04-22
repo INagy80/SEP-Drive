ecc5eff3633973bdf67d643ec5103e4f730b38c3
Ibrahim Nagi Ahmed Mohamed Sayed <ibrahim.nagiahmedmohamedsayed@stud.uni-due.de>
2025-04-23 00:45:39 +0200

    I have added the backend folder and its structure, also completed the pom.xml file with all the needed dependency

---
+++ backend/src/main/java/com/example/SEPDrive/SepDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.SEPDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepDriveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/SEPDrive/SepDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SEPDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
