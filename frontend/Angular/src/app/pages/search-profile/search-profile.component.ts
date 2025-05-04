bfd2fb346dd079d4502b45e424fe73fe6096a890
rawan almalla <rawan.almalla@stud.uni-due.de>
2025-05-04 20:22:14 +0200

    added user-profile and search-profile pages

---
+++ frontend/Angular/src/app/pages/search-profile/search-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-profile',
+  imports: [],
+  templateUrl: './search-profile.component.html',
+  styleUrl: './search-profile.component.scss'
+})
+export class SearchProfileComponent {
+
+}
