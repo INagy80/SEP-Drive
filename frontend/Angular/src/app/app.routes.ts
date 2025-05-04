cc02cc39487a120f12c090859b918809b9a0f14e
rawan almalla <rawan.almalla@stud.uni-due.de>
2025-05-04 19:01:59 +0200

    added an empty Register page

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
 ];
