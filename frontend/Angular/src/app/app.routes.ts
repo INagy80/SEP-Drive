2a311579aba5ae4121dd3c7541b13771a4e71eda
Aya Almalla <aya.almalla@stud.uni-due.de>
2025-05-04 20:39:52 +0200

    Added login and 2FA routes

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TwoFAComponent } from './pages/two-fa/two-fa.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'twofa', component: TwoFAComponent },
   { path: '**', redirectTo: '' }
 ];
