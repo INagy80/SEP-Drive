0f38b579b51d390719105f27be60613fd4d014eb
rawan almalla <rawan.almalla@stud.uni-due.de>
2025-05-04 20:59:33 +0200

    Added  user-profile and search-profile routes

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
-import { TwoFAComponent } from './pages/two-fa/two-fa.component';
+import { TwoFAComponent } from './pages/two-fa/two-fa.component'; 
+import { SearchProfileComponent } from './pages/search-profile/search-profile.component'; 
+import { UserProfileComponent } from './pages/user-profile/user-profile.component'; 
+
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'twofa', component: TwoFAComponent },
+  { path: 'search-profile', component: SearchProfileComponent },
+  { path: 'user-profile', component: UserProfileComponent },
   { path: '**', redirectTo: '' }
 ];
