3491e69d51c936d1b93a2e8fe772377dc07e5847
Ibrahim Nagi Ahmed Mohamed Sayed <ibrahim.nagiahmedmohamedsayed@stud.uni-due.de>
2025-05-05 17:46:01 +0200

    create custum userDetailsService

---
+++ backend/src/main/java/com/example/SEPDrive/service/myUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.example.SEPDrive.service;
+
+import com.example.SEPDrive.exceptions.userNotFoundException;
+import com.example.SEPDrive.model.user;
+import com.example.SEPDrive.model.userPrincipal;
+import com.example.SEPDrive.repository.userDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class myUserDetailsService implements UserDetailsService {
+
+
+
+    @Autowired
+    private userDAO userDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       user user = userDAO.findByUserName(username);
+
+       if(user == null) {
+           throw new userNotFoundException("User not found");
+       }
+
+        return new userPrincipal(user);
+    }
+}
