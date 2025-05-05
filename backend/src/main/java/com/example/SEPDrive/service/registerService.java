2ab668459621c57d66df18f0add3d9809e13cd9e
BasharHerzallah <bashar.herzallah@uni-due.de>
2025-05-05 15:36:05 +0200

    Added the registerService for business Layer

---
+++ backend/src/main/java/com/example/SEPDrive/service/registerService.java
@@ -0,0 +1,50 @@
+package com.example.SEPDrive.service;
+
+import com.example.SEPDrive.controller.AuthenticatinResponse;
+import com.example.SEPDrive.exceptions.duplicatResourceException;
+import com.example.SEPDrive.model.user;
+import com.example.SEPDrive.repository.userDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class registerService {
+
+    @Autowired
+    private  userDAO userDao;
+
+    @Autowired
+    private emailSenderService emailSenderService;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+
+
+
+    //@EventListener(ApplicationReadyEvent.class)
+    public AuthenticatinResponse register(user user) {
+        if (userDao.existsUserByEmail(user.getEmail())) {
+            throw new duplicatResourceException("This email already exists");
+        } else if (userDao.existsUserByUserName(user.getUserName())) {
+            throw new duplicatResourceException("This username already exists");
+
+            //add
+        }else{
+            user.setPassword(encoder.encode(user.getPassword()));
+            Integer FA2 = new Random().nextInt(900000) + 100000;
+            emailSenderService.sendEmail(user.getEmail(), "SEPDrive Verification Code", "Your verification code is: " + FA2);
+            userDao.save(user);
+
+            return null;
+
+
+        }
+    }
+
+}
