8eb1d1d6093ed068727edb985b7a88be0c30a805
Sara Akgüvercin <Sara Akgvercin>
2025-05-06 22:30:40 +0200

    ich habe die registrierungsseite erstellt

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {WelcomeComponent} from '../welcome/welcome.component'
 import {LoginComponent} from '../login/login.component'
 import {TwoFAComponent } from '../two-fa/two-fa.component';
+import {RegisterComponent} from '../register/register.component';
 
 
 export const routes: Routes = [
@@ -9,5 +10,5 @@ export const routes: Routes = [
   { path: 'home', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: '2fa', component: TwoFAComponent },
-
+  { path: 'register', component : RegisterComponent}
 ];
---
+++ frontend/Angular/src/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+  //styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  zeigeFeld: boolean = false;
+}
