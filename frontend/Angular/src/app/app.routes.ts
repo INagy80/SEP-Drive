ad274e5abcb0b6e7eac22a4a6927bc88e4b8c830
Aya Almalla <aya.almalla@stud.uni-due.de>
2025-05-06 12:23:02 +0200

    Added a welcome page with styling and basic HTML setup for login and 2fa pages

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {WelcomeComponent} from '../welcome/welcome.component'
+import {LoginComponent} from '../login/login.component'
+import {TwoFAComponent } from '../two-fa/two-fa.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '2fa', component: TwoFAComponent },
+
+];
