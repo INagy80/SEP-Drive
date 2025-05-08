7c5847ddbc379420c85b02ad32fec933fb4678f0
Sara Akgüvercin <Sara Akgvercin>
2025-05-08 13:19:49 +0200

    alles speichern

--- frontend/Angular/src/register/register.component.ts
+++ frontend/Angular/src/register/register.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
-  //styleUrls: ['./register.component.scss']
+  styleUrl: './register.component.scss',
+  imports : [
+    NgIf
+  ]
+
 })
 export class RegisterComponent {
   zeigeFeld: boolean = false;
+  profilbildDatei: File | null = null;
+
+  profilbildVorschauUrl: string | null = null;
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.profilbildDatei = file;
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.profilbildVorschauUrl = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
