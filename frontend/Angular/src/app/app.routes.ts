bd77cb7d2e70a8c8c9e71aba271c0890ac4f4af9
Aya Almalla <aya.almalla@stud.uni-due.de>
2025-05-12 15:06:07 +0200

    Added an empty forgot password page

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import {WelcomeComponent} from '../welcome/welcome.component'
 import {LoginComponent} from '../login/login.component'
 import {TwoFAComponent } from '../two-fa/two-fa.component';
+import {ForgotPasswordComponent } from '../forgot-password/forgot-password.component';
+
 
 
 export const routes: Routes = [
@@ -9,5 +11,5 @@ export const routes: Routes = [
   { path: 'home', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: '2fa', component: TwoFAComponent },
-
+  { path: 'forgot-password', component:ForgotPasswordComponent },
 ];
--- frontend/Angular/src/login/login.component.ts
+++ frontend/Angular/src/login/login.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {Password} from 'primeng/password';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [
+    Password,
+    FormsModule
+  ],
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username: string = ``;
+  password: string = ``;
   constructor(private router: Router) {}
+
 
   onLogin() {
     console.log("Login wurde geklickt");
     this.router.navigate([`/2fa`]);
   }
+
+  onRegister() {
+    console.log("Register wurde geklickt");
+    this.router.navigate([`/register`]);
+  }
+
 }
