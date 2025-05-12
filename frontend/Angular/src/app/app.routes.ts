c1d96f3f76bd04bc432ba324d03043b9693ef306
Aya Almalla <aya.almalla@stud.uni-due.de>
2025-05-12 17:31:35 +0200

    Added Routes to the maps page and the Fahranfragen page

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { MapComponent } from '../map/map.component';
+import { FahranfragenComponent } from '../fahranfragen/fahranfragen.component';
+export const routes: Routes = [
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
+  { path: 'map', component: MapComponent },
+  { path: 'fahranfragen', component: FahranfragenComponent },
+];
