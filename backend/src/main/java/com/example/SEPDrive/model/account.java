ae18a10a89e44c8bab37bb00b173d2c3704c4c6f
BasharHerzallah <bashar.herzallah@uni-due.de>
2025-06-12 13:03:34 +0200

    The Account Entity are done !

---
+++ backend/src/main/java/com/example/SEPDrive/model/account.java
@@ -0,0 +1,47 @@
+package com.example.SEPDrive.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(mappedBy = "account")
+    private user user;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<transaction> transactions = new ArrayList<>();
+
+    public void addTransaction(transaction tx) {
+        transactions.add(tx);
+        tx.setAccount(this);
+    }
+
+    public BigDecimal getBalance() {
+        return transactions.stream()
+                .map(transaction::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public List<transaction> getTransactions() {
+        return transactions;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public user getUser() {
+        return user;
+    }
+
+    public void setUser(user user) {
+        this.user = user;
+    }
+}
