bb403f0e0af11675410691309da6ad29b21aa22b
Ibrahim Nagi Ahmed Mohamed Sayed <ibrahim.nagiahmedmohamedsayed@stud.uni-due.de>
2025-06-17 21:59:19 +0200

    djjhd

--- backend/src/test/java/com/example/SEPDrive/service/geldKontoServiceDiffblueTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.SEPDrive.service;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-
-class geldKontoServiceDiffblueTest {
-
-}
