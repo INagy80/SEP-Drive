e44fbdf400daa1ecc149b199701d4a01d7ba7dea
rawan almalla <rawan.almalla@stud.uni-due.de>
2025-07-06 21:46:28 +0200

    Component fahrten-anaylse erstellt

---
+++ frontend/Angular/src/app/components/fahrten-analyse/fahrten-analyse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fahrten-analyse',
+  imports: [],
+  templateUrl: './fahrten-analyse.component.html',
+  styleUrl: './fahrten-analyse.component.scss'
+})
+export class FahrtenAnalyseComponent {
+
+}
